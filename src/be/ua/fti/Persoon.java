0750e87eae3b43dade06aad996fb340999d7ba24
Filip Van der Schueren <filip.vanderschueren@uantwerpen.be>
2018-10-04 23:11:51 +0200

    added class Persoon

---
+++ src/be/ua/fti/Persoon.java
@@ -0,0 +1,24 @@
+package be.ua.fti;
+
+public class Persoon {
+    private String naam;
+
+    public Persoon(String naam) {
+        this.naam = naam;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    @Override
+    public String toString() {
+        return "Persoon{" +
+                "naam='" + naam + '\'' +
+                '}';
+    }
+}
